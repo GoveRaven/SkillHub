f2d8a56ac3dc2dc3b3926e9583c58062f9a92cef
Даня <daniilevgen79@gmail.com>
2026-04-11 12:26:30 +0500

    change mx

--- apps/web/app/(home)/popular-products.tsx
+++ apps/web/app/(home)/popular-products.tsx
@@ -10,7 +10,7 @@ interface IPopularProducts {
 export default function PopularProducts({ products }: IPopularProducts) {
   const popularProduect = products.slice(0, 6)
   return (
-    <section className='py-20 px-8 mx-auto'>
+    <section className='py-20 px-8 mx-75'>
       <div className='text-center mb-16'>
         <h1 className='text-5xl font-bold text-gray-800 mb-6'>Курсы</h1>
         <p className='text-xl text-gray-600 max-w-2xl mx-auto'>
