27113ea7156af85556785ca47974c4550711ff0f
Даня <daniilevgen79@gmail.com>
2026-04-11 11:58:14 +0500

    клиент для статьей Strapi

--- apps/web/api/config.ts
+++ apps/web/api/config.ts
@@ -2,6 +2,7 @@ export const BASE_URL = 'http://localhost:1337/api';
 
 export const ENDPOINT = {
   COURSE: '/courses',
+  ARTICLE: '/articles'
 };
 
 export const POPULATE_ALL = '?populate=*';
---
+++ apps/web/api/utils/articles.ts
@@ -0,0 +1,15 @@
+import { BASE_URL, ENDPOINT, POPULATE_ALL } from '../config';
+
+export async function fetchArticles() {
+  const { data } = await fetch(`${BASE_URL}${ENDPOINT.ARTICLE}`).then((res) =>
+    res.json(),
+  );
+  return data;
+}
+
+export async function fetchArticle(documentId: string) {
+  const { data } = await fetch(
+    `${BASE_URL}${ENDPOINT.ARTICLE}/${documentId}${POPULATE_ALL}`,
+  ).then((res) => res.json());
+  return data;
+}
