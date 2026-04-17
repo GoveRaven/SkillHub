3a15e37c6a72e8b2c069c1ae1af14e09d1d34128
Даня <daniilevgen79@gmail.com>
2026-04-17 22:52:56 +0500

    добавил отступы

--- apps/web/app/catalog/page.tsx
+++ apps/web/app/catalog/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useState } from 'react';
 import Courses from './courses';
 import Filters from './filters';
--- apps/web/app/content/page.tsx
+++ apps/web/app/content/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import Loader from '@/component/loader';
 import Link from 'next/link';
 import Error from '@/component/error';
